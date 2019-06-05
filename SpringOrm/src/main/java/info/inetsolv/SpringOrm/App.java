073ae7b29913033eb156996ae6a4d9ef6aa620ce
Krishna <Krishna@DESKTOP-32OBQ1J>
2019-06-05 10:43:39 +0530

    bye statement added

--- SpringOrm/src/main/java/info/inetsolv/SpringOrm/App.java
+++ SpringOrm/src/main/java/info/inetsolv/SpringOrm/App.java
@@ -24,6 +24,8 @@ public static void main(String[] args) {
 	System.out.println(employee.getName());
 	  System.out.println("no reecords");
 	  System.out.println("hi");
+	  
+	  System.out.println("bye");
 	
 	((AbstractApplicationContext)container).close();
 }
